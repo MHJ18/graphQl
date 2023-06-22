ad09209c397926a8a9e14f33215cc8afac6e2e95
hassaan1814 <hassaanjaved82@triconv.com>
2023-06-23 00:13:11 +0500

    GraphQL CRUD

---
+++ testdata.js
@@ -0,0 +1,25 @@
+export const User = [
+  {
+    id: "1",
+    name: "Hassaan",
+    email: "hassaanjabed81@gmail.com",
+    password: "123345",
+  },
+  {
+    id: "2",
+    name: "Hassaan2",
+    email: "hassaanjabed82@gmail.com",
+    password: "1233456",
+  },
+];
+export const author = [
+  {
+    by: "1",
+    name: "asad",
+  },
+  {
+    by: "1",
+    name: "zeshan",
+  },
+  { by: "2", name: "hassaan" },
+];
